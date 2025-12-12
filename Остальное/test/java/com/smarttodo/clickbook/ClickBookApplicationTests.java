1cb0ccf5211c01abdb7e04aa5079843b9b5c1a3c
Nadezhda490 <lebedinskaya200303@gmail.com>
2025-12-12 08:29:10 +0300

    Add files via upload

---
+++ Остальное/test/java/com/smarttodo/clickbook/ClickBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smarttodo.clickbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClickBookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
